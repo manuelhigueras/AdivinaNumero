ce00004b8bc5dc687bc2ffc8d612ae3d28976ce2
user <user@PC514>
2020-11-18 12:01:15 +0100

    Push de todo 5

---
+++ src/main/java/com/juego/domain/GameMenu.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.juego.domain;
+
+/**
+ *
+ * @author user
+ */
+public class GameMenu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Juego num = new Juego();
+        
+        int valor = 5;
+        boolean acerto = num.comprobar(valor);
+        /*
+        if(acierto){
+            System.out.println("...ACERTASTES CRACK");
+        }
+        else{
+            System.out.println("...NO ACERTO");
+        }
+        */
+    }
+    
+}
